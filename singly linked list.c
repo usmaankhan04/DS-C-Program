6fc1d9a4b776d258e0545ee0de635f41e1b92aa6
usmaankhan04 <usmaankhan04@icloud.com>
2025-12-14 22:53:41 +0530

    Add files via upload

---
+++ singly linked list.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void insert(int x)
+{
+    struct node *n, *t;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = x;
+    n->next = NULL;
+
+    if (head == NULL)
+        head = n;
+    else
+    {
+        t = head;
+        while (t->next != NULL)
+            t = t->next;
+        t->next = n;
+    }
+}
+
+void display()
+{
+    struct node *t = head;
+    while (t != NULL)
+    {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+}
+
+int main()
+{
+    insert(10);
+    insert(20);
+    insert(30);
+    display();
+    return 0;
+}
